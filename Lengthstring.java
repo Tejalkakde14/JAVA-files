cafc6372ce9a16336f0a4f63e60ce9dbbd52e071
Tejalkakde14 <151601854+Tejalkakde14@users.noreply.github.com>
2024-01-18 22:29:05 +0530

    Add files via upload

---
+++ Lengthstring.java
@@ -0,0 +1,9 @@
+//Java Program to Find the Length of a String
+
+public class Lengthstring{  
+
+public static void main(String args[]){  
+String str="welcome to lala land";  
+  
+System.out.println("string length is: "+str.length());  
+}}  
